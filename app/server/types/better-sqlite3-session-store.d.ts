1097657c21aa8392faad4320d91cf69526a4fac3
Catskill909 <36337311+Catskill909@users.noreply.github.com>
2026-03-16 14:43:46 -0400

    feat: add type definitions for better-sqlite3-session-store module

---
+++ app/server/types/better-sqlite3-session-store.d.ts
@@ -0,0 +1,12 @@
+declare module 'better-sqlite3-session-store' {
+    import session from 'express-session';
+
+    function BetterSqlite3StoreFactory(
+        session: typeof import('express-session')
+    ): new (options: {
+        client: import('better-sqlite3').Database;
+        expired?: { clear?: boolean; intervalMs?: number };
+    }) => session.Store;
+
+    export = BetterSqlite3StoreFactory;
+}
